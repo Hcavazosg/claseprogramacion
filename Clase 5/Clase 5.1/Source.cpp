2ac474d15c1447ee99bd5717bd2677fd467927d8
hcavazosg <hectorcav9812@gmail.com>
2022-04-26 15:31:38 -0500

    Clase 5

---
+++ Clase 5/Clase 5.1/Source.cpp
@@ -0,0 +1,105 @@
+/*
+    Curso C++
+    Condicionales
+*/
+
+#include <iostream>
+using namespace std;
+
+
+//void main() {
+//
+//    char Letra;
+//
+//    cout << "Ingrese la letra A" << endl;
+//    cin >> Letra;
+//
+//    if (Letra == 'A') {
+//
+//        cout << "Se ingreso la letra A" << endl;
+//
+//
+//    }
+//    else {
+//        cout << "Se ingreso la letra incorrecta" << endl;
+//    }
+//
+//    system("pause");
+//}
+
+//int numero1;
+//int numero2;
+//
+//cout << "Ingrese el primer numero" << endl;
+//cin >> numero1;
+//
+//cout << "Ingrese el segundo numero" << endl;
+//cin >> numero2;
+//
+///*
+//== Igual
+//!= Diferente
+//< Menor que
+//> Mayor que
+//<= Menor igual que
+//>= Mayor igual que
+//*/
+//if (numero1 != numero2) {
+//    cout << "El numero " << numero1 << " es diferente de " << numero2 << endl;
+//}
+//else {
+//    cout << "El numero " << numero1 << " es igual a " << numero2 << endl;
+//}
+
+/*
+    Declarar 3 variables
+    Pedirle al usuario que ingrese 3 números
+    Compararlos (   if-else   )
+    Imprimir el resultado
+*/
+
+
+//void main() {
+//    int numero1;
+//    int numero2;
+//    int numero3;
+//
+//    cout << "Ingrese el primer numero" << endl;
+//    cin >> numero1;
+//
+//    cout << "Ingrese el segundo numero" << endl;
+//    cin >> numero2;
+//
+//    cout << "Ingrese el segundo numero" << endl;
+//    cin >> numero3;
+//
+//    if (numero1>numero2 && numero1>numero3){
+//        cout << "El numero " << numero1 << " es mayor " << endl;
+//    }
+//
+//    if(numero2>numero1 && numero2>numero3) {
+//        cout << "El numero " << numero2 << " es mayor " << endl;
+//
+//    }
+//
+//    if (numero3 > numero1 && numero3 > numero2) {
+//        cout << "El numero " << numero3 << " es mayor " << endl;
+//    }
+//}
+
+//void main() {
+//        //Programa que determina si un número es par o impar
+//
+//        int num;
+//
+//        cout << "Escriba un numero: ";
+//        cin >> num;
+//
+//        if (num % 2 == 0) {
+//            cout << "El numero es Par\n";
+//        }
+//        else {
+//            cout << "El numero es Impar\n";
+//        }
+//    }
+
