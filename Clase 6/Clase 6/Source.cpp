5d1677540bbf147566802bfac7319c11240f6353
hcavazosg <hectorcav9812@gmail.com>
2022-05-02 17:18:18 -0500

    Clase 6

---
+++ Clase 6/Clase 6/Source.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+using namespace std;
+
+
+void main() {
+    //// Ciclo es una sección de código que se va a repetir mientras una condición sea cumplida. 
+
+    //// For, While, Do-While 
+
+    //for (int i = 0; i < 5; i++) {
+    //    // - Una variable que irá aumentando o disminuyendo
+    //    // - Una condición a cumplir 
+    //    // - El incremento o decremento de nuestra variable
+    //    cout << "Numero: " << i << endl;
+    //}
+
+    //cout << endl;
+
+    //int i = 0;
+    //while (i < 6) {
+
+    //    cout << "Numero: " << i << endl;
+    //    i++;
+    //}
+
+    //cout << endl;
+
+
+    //int numero;
+    //do {
+    //    cout << "Ingrese un numero igual o mayor 100" << endl;
+    //    cin >> numero;
+
+    //} while (numero == 3);
+
+
+
+    //system("pause");
+
+    //char i = 65;
+    //for (i = 65; i < 91; i++) {
+    //    cout << "Letra: " << i << endl;
+    //    
+    //}
+
+    //cout << endl; 
+  /* 
+    int resultado = 0; 
+    int i = 1;
+    for (i = 1; i < 11; i++) {
+        cout << "Numero Impar: " << i << endl;
+        resultado = resultado + i;
+        i++;
+    }
+    cout << "Resultado " << resultado << endl;*/
+
+    //Calcular el exponente de un número utilizando ciclos
+
+    //    Por ejemplo, pedir al usuario que ingrese el número que vas a elevar a la n potencia y después ingresar n
+    //    Mostrar el resultado final
+
+
+
+
+ /*   Crear un programa que recree la sucesión de fibonacci, el usuario indicará cuántos 
+ números van a mostrarse, por ejemplo, si el usuario ingresa 5, se mostrará : 0, 1, 1, 2, 3*/
+
+
+    
+        //int first_num = 0, second_num = 1, third_num, i, num;
+        //cout << "Enter random number to print fibonacci series:";
+        //cin >> num;
+        //cout << "Fibonacci Series for a given number:" << endl;
+        //cout << first_num << endl;
+        //cout << second_num << endl;
+        //for (i = 2; i < num; ++i) //loop will starts from 2 because we have printed 0 and 1 before
+        //{
+        //    third_num = first_num + second_num;
+        //    cout << third_num << endl;
+        //    first_num = second_num;
+        //    second_num = third_num;
+        //}
+    
+ 
+        //int base, exponent, power, i;
+
+        ////Reading base & exponent
+        //cout << "Enter base: ";
+        //cin >> base;
+        //cout << "Enter exponent: ";
+        //cin >> exponent;
+
+        //power = 1;
+        //i = 1;
+        ////caculatinh power of given number
+        //while (i <= exponent)
+        //{
+        //    power = power * base;
+        //    i++;
+        //}
+        //cout << "Power of " << base << " is: " << power;
+
+       
+    
+}
